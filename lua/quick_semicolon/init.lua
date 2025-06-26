b88a18b4fc67692ea1ff4e1396d0f45044dcfc81
lenwood <lenwood081@gmail.com>
2025-06-27 00:14:08 +1000

    finished functionality, added disable and changing of key binding

--- lua/quick_semicolon/init.lua
+++ lua/quick_semicolon/init.lua
@@ -1,18 +1,29 @@
 local M = {}
 
-function M.setup()
+function M.setup(opts)
+  opts = opts or {}
+
+  -- set options
+  if not opts.disable then
+    opts.disable = false
+  end
+
   -- default keymap
-  vim.keymap.set(
-      'n',
-      '<leader>;',
-      M.quickSemicolon,
-      { desc = 'Custom: Add semicolon to end of line' }
-  )
+  if opts.disable == false then
+    vim.keymap.set(
+        'n',
+        '<leader>;',
+        M.quickSemicolon,
+        { desc = 'Custom: Add semicolon to end of line', noremap = true }
+    )
+  end
 end
 
 function M.quickSemicolon()
   -- for custom keymap
-  return [[A;<Esc>]]
+  return vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('A;<Esc>', true, false, true), 'n', false)
 end
+
+
 
 return M
--- plugin/quick_semicolon.lua
+++
@@ -1,1 +0,0 @@
-require("quick_semicolon").setup()
